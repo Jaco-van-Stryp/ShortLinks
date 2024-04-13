2506cf5cf65e42489195a4e66ac159860158fff1
Jaco van Stryp <jaco.vanstryp@sybrin.com>
2024-04-13 19:38:28 +0200

    Added Link Page

--- ShortLinks_Final/Shortlink/src/app/link-page/link-page.component.ts
+++ ShortLinks_Final/Shortlink/src/app/link-page/link-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { URL } from '../_interfaces/url';
+import { URLService } from '../_services/url.service';
 @Component({
   selector: 'app-link-page',
   templateUrl: './link-page.component.html',
-  styleUrls: ['./link-page.component.scss']
+  styleUrls: ['./link-page.component.scss'],
 })
 export class LinkPageComponent {
+  Links: URL[] = [];
 
+  constructor(private URLService: URLService) {}
+
+  loadURLS() {
+    this.URLService.GetLinksForUser().subscribe({
+      next: (res: URL[] | any) => {
+        this.Links = res;
+      },
+    });
+  }
 }
