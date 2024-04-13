1947e9548cacfd9e3a31fc44a6884ee2793c47a1
Jaco van Stryp <jaco.vanstryp@sybrin.com>
2024-04-13 19:28:24 +0200

    Added Registration

--- ShortLinks_Final/Shortlink/src/app/register/register.component.ts
+++ ShortLinks_Final/Shortlink/src/app/register/register.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Account } from '../_interfaces/account';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  User: Account = {} as Account;
 
+  constructor(
+    private AccountService: AccountService,
+    private Toastr: ToastrService
+  ) {}
+
+  Register(User: Account) {
+    this.AccountService.Register(User).subscribe({
+      next: (res: any) => {
+        this.Toastr.success('Registration Successful');
+        var currentUser = this.AccountService.GetCurrentUser();
+        console.log(currentUser.token);
+      },
+      error: () => {
+        this.Toastr.error('User Already Exists');
+      },
+    });
+  }
 }
