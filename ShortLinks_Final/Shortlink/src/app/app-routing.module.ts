d57a16a1d59f62a3d88347a4e89f3b20193ab016
Jaco van Stryp <jaco.vanstryp@sybrin.com>
2024-04-14 21:36:46 +0200

    Improved Login and Landing page

--- ShortLinks_Final/Shortlink/src/app/app-routing.module.ts
+++ ShortLinks_Final/Shortlink/src/app/app-routing.module.ts
@@ -4,19 +4,17 @@ import { LinkRouterComponent } from './link-router/link-router.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LinkPageComponent } from './link-page/link-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
-  },
-    {
-    path: 'Home',
-    component: HomeComponent,
-  },
-  {
     path: 's/:ShortURL',
     component: LinkRouterComponent,
+  },
+  {
+    path: 'Home',
+    component: HomeComponent,
   },
   {
     path: 'NotFound',
@@ -25,6 +23,18 @@ const routes: Routes = [
   {
     path: 'MyLinks',
     component: LinkPageComponent,
+  },
+  {
+    path: 'Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'Register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
